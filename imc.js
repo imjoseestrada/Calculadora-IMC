891a5c0c612ab6103289f881ac193abf528ef5ec
Jose Estrada <j.estrada49@icloud.com>
2020-02-21 20:04:00 -0700

    Se ajusta para sobreescribir leyenda
    Se agregan datos de autor y se ajusta leyenda para que en lugar de concatenarla, sobreescriba al resultado anterior (si hace más de un cálculo)

--- imc.js
+++ imc.js
@@ -15,12 +15,12 @@ function obtenerResultado() {
     var resultado = document.getElementsByClassName('resultado')[0]
 
     if (imc<18.5) {
-        resultado.innerHTML += '<label>Peso inferior al normal</label>'
+        resultado.innerHTML = '<label>Peso inferior al normal</label>'
     } else if ((imc >= 18.5) && (imc < 25)) {
-        resultado.innerHTML += '<label>Normal</label>'
+        resultado.innerHTML = '<label>Normal</label>'
     } else if ((imc >= 25) && (imc < 30)) {
-        resultado.innerHTML += '<label>Peso superior al normal</label>'
+        resultado.innerHTML = '<label>Peso superior al normal</label>'
     } else if (imc >= 30) {
-        resultado.innerHTML += '<label>Obesidad</label>'
+        resultado.innerHTML = '<label>Obesidad</label>'
     }
 }
